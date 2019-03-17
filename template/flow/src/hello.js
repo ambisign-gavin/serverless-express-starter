7dd4b157f739d631cabbaabab02923cadca2faab
kuku8990 <kuku8990@gmail.com>
2019-03-17 20:13:44 +0800

    Add the template of flow

---
+++ template/flow/src/hello.js
@@ -0,0 +1,15 @@
+// @flow
+import express, { type $Request, type $Response } from 'express';
+
+
+const helloController = () => {
+    const app = express();
+
+    app.get('/hello', (req: $Request, res: $Response) => {
+        res.status(200).send('Hello!');
+    });
+
+    return app;
+};
+
+export default helloController;
---
+++ template/flow/src/index.js
@@ -0,0 +1,5 @@
+// @flow
+import serverless from 'serverless-http';
+import helloController from './hello';
+
+export const hello = serverless(helloController());
