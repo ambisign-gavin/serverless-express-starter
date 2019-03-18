3252e90f59f23b9db464256f70ae10ac186b8826
kuku8990 <kuku8990@gmail.com>
2019-03-18 12:45:43 +0800

    Add required tools checker

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 // @flow
 import { initProject } from './task';
 import yargs, { argv } from 'yargs';
+import execa from 'execa';
+import chalk from 'chalk';
 
-export function run() {
+export async function run() {
     if (Object.keys(argv).length > 2) {
         console.log('\nUsage: serverless-express-starter <command>\n');
         console.log('where <command> is one of:\n');
         yargs.showHelp();
         return;
     }
+    let isRequiredTool = await isRequiredToolHasInstalled();
+    if (!isRequiredTool) {
+        return;
+    }
     initProject();
+}
+
+async function isRequiredToolHasInstalled(): Promise<boolean> {
+    try {
+        await execa.shell('which', [
+            'git'
+        ]);
+    } catch (error) {
+        console.log(chalk.red('serverless-express-started need installed git.'));
+        return false;
+    }
+    return true;
 }
