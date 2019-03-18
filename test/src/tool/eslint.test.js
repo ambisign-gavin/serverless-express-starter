0e66fef88cc4984311690d063fa0348c07ba1901
kuku8990 <kuku8990@gmail.com>
2019-03-18 22:00:37 +0800

    Disable jest auto clear mocks

--- test/src/tool/eslint.test.js
+++ test/src/tool/eslint.test.js
@@ -10,6 +10,10 @@ jest.mock('fs');
 
 describe('Eslint', () => {
 
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should get install packages correct', () => {
         expect(eslint.getInstallPackages()).toEqual([
             'eslint@^5.0.0',
