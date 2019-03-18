12e1402f86edbd1e0e530e039ccc29a63c4115b3
kuku8990 <kuku8990@gmail.com>
2019-03-18 11:32:16 +0800

    Add command options supported

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 // @flow
 import { initProject } from './task';
+import yargs, { argv } from 'yargs';
 
 export function run() {
+    if (Object.keys(argv).length > 2) {
+        console.log('\nUsage: serverless-express-starter <command>\n');
+        console.log('where <command> is one of:\n');
+        yargs.showHelp();
+        return;
+    }
     initProject();
 }
