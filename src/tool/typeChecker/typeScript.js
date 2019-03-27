4e4d84e3b51f252568ce2214186af78640ccd5fc
kuku8990 <kuku8990@gmail.com>
2019-03-27 20:52:48 +0800

    Add typeScript class

---
+++ src/tool/typeChecker/typeScript.js
@@ -0,0 +1,49 @@
+// @flow
+import { TypeChecker } from './interface';
+
+export class TypeScriptTypeChecker implements TypeChecker {
+    generateTemplatePath(): string {
+        return 'template/typescript/';
+    }
+
+    getInstallPackages(): Array<string> {
+        //need to install aws-lambda to avoid this issue: https://github.com/dougmoscrop/serverless-http/issues/78
+        return [
+            '@types/aws-lambda@^8.0.0', 
+            '@types/express@^4.0.0',
+            '@typescript-eslint/eslint-plugin@^1.0.0',
+            '@typescript-eslint/parser@^1.0.0',
+            'typescript@^3.0.0',
+        ];
+    }
+
+    runExtraSettings(projectPath: string): Promise<void> {
+        return Promise.resolve();
+    }
+
+    injectSettingsToEslintConfig(config: Object): Object {        
+        let newConfig = {
+            ...config,
+            parser: '@typescript-eslint/parser',
+            plugins: [
+                ...this._parseEslintConfigPropertyToArray(config.plugins),
+                '@typescript-eslint'
+            ],
+            extends: [
+                ...this._parseEslintConfigPropertyToArray(config.extends),
+                'plugin:@typescript-eslint/recommended'
+            ]
+        };
+        return newConfig;
+    }
+
+    _parseEslintConfigPropertyToArray(property: ?string | ?Array<string>): Array<string> {
+        if (property == null) {
+            return [];
+        }
+        if (Array.isArray(property)) {
+            return property;
+        }
+        return [property];
+    }
+}
---
+++ test/src/tool/typeChecker/typeScript.test.js
@@ -0,0 +1,50 @@
+// @flow
+import { TypeScriptTypeChecker } from '../../../../src/tool/typeChecker/typeScript';
+
+jest.mock('execa');
+
+describe('Flow type checker', () => {
+
+    const typeChecker = new TypeScriptTypeChecker();
+
+    it('should generate template path correct', () => {
+        expect(typeChecker.generateTemplatePath()).toEqual('template/typescript/');
+    });
+
+    it('should get install packages correct', () => {
+        expect(typeChecker.getInstallPackages()).toEqual([
+            '@types/aws-lambda@^8.0.0', 
+            '@types/express@^4.0.0',
+            '@typescript-eslint/eslint-plugin@^1.0.0',
+            '@typescript-eslint/parser@^1.0.0',
+            'typescript@^3.0.0',
+        ]);
+    });
+
+    it('should run extra settings correct', async () => {
+        await expect(typeChecker.runExtraSettings('path')).toEqual(Promise.resolve());
+    });
+
+    it('should injection settings to eslint config correct', () => {
+        let config = {
+            env: {
+                es6: true,
+                node: true
+            },
+            extends: 'eslint:recommended'
+        };
+
+        expect(typeChecker.injectSettingsToEslintConfig(config)).toEqual({
+            ...config,
+            parser: '@typescript-eslint/parser',
+            plugins: [
+                '@typescript-eslint'
+            ],
+            extends: [
+                'eslint:recommended', 
+                'plugin:@typescript-eslint/recommended'
+            ]
+        });
+    });
+
+});
